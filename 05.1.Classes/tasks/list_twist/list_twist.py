19f01d2ec120ccce899e3ad413697598deab1679
duffily <rblashchuk@gmail.com>
2023-10-23 23:06:51 +0300

    list

--- 05.1.Classes/tasks/list_twist/list_twist.py
+++ 05.1.Classes/tasks/list_twist/list_twist.py
@@ -3,33 +3,33 @@ import typing as tp
 
 
 class ListTwist(UserList[tp.Any]):
-    def __init__(self, data=None) -> None:
+    def __init__(self, data=None):
         super().__init__(data if data is not None else [])
 
-    def __getattr__(self, name) -> None:
+    def __getattr__(self, name):
         if name == 'reversed' or name == 'R':
             return self.data[::-1]
         elif name == 'first' or name == 'F':
             if not self.data:
-                raise AttributeError("mpty")
+                raise AttributeError("empty")
             return self.data[0]
         elif name == 'last' or name == 'L':
             if not self.data:
-                raise AttributeError("mpty")
+                raise AttributeError("empty")
             return self.data[-1]
         elif name == 'size' or name == 'S':
             return len(self.data)
         else:
             raise AttributeError(f"no '{name}'")
 
-    def __setattr__(self, name, value) -> None:
+    def __setattr__(self, name, value):
         if name == 'first' or name == 'F':
             if not self.data:
                 raise AttributeError("empty")
             self.data[0] = value
         elif name == 'last' or name == 'L':
             if not self.data:
-                raise AttributeError("mpty")
+                raise AttributeError("empty")
             self.data[-1] = value
         elif name == 'size' or name == 'S':
             if value < 0:
@@ -39,4 +39,4 @@ class ListTwist(UserList[tp.Any]):
             elif value > len(self.data):
                 self.data.extend([None] * (value - len(self.data)))
         else:
-            super().__setattr__(name, value)
+                super().__setattr__(name, value)
