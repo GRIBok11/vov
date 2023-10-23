5e4f8359a57adcb3718e71c708800f6dfadca327
duffily <rblashchuk@gmail.com>
2023-10-23 22:35:27 +0300

    ready

--- 05.1.Classes/tasks/orders/orders.py
+++ 05.1.Classes/tasks/orders/orders.py
@@ -1,38 +1,63 @@
+import typing as tp
+
+from abc import ABC, abstractmethod, abstractclassmethod
 from dataclasses import dataclass, field, InitVar
-from abc import ABC
+
+a = 15
 
 
-DISCOUNT_PERCENTS = 15
+@dataclass(frozen=True, order=True)
+class Item(object):
+    title: str = field(compare=True)
+    item_id: int = field(compare=False)
+    cost: int = field(compare=True)
 
+    def __post_init__(self) -> None:
+        if self.cost <= 0 or not len(str(self.title)):
+            raise AssertionError()
 
-class Item:
-    # note: you might want to change the order of fields
-    cost: int
-    title: str
-    item_id: int
-
-
-# You may set `# type: ignore` on this class
-# It is [a really old issue](https://github.com/python/mypy/issues/5374)
-# But seems to be solved
 @dataclass
 class Position(ABC):
     item: Item
 
-    def cost(self):
-        pass
+    @abstractclassmethod
+    def cost(self) -> tp.Any:
+        return self.item.cost
 
-
+@dataclass
 class CountedPosition(Position):
-    count: int
+    count: float = 1.0
+
+    @property
+    def cost(self) -> tp.Any:
+        return self.count * self.item.cost
 
 
+@dataclass
 class WeightedPosition(Position):
-    weight: float
+    weight: float = 1.0
+
+    @property
+    def cost(self) -> tp.Any:
+        return self.weight * self.item.cost
 
 
+@dataclass
 class Order:
-    order_id: int
-    positions: list[Position]
-    cost: int
-    have_promo: bool
+    order_id: int = 0
+    positions: list[Position] = field(default_factory=list[tp.Any])
+    cost: int = 0
+    have_promo: InitVar[tp.Any] = False
+
+    def __post_init__(self, have_promo: tp.Any) -> None:
+        op = 0
+        for p in self.positions:
+            op += p.cost
+
+        if have_promo:
+            dis = 1 - a / 100
+        else:
+            dis = 1
+
+        self.cost = int(op * dis)
+
