7ef66906d08b0a1861e822e61dbbb6ec1527b60e
duffily <rblashchuk@gmail.com>
2023-10-23 19:01:18 +0300

    05.01

--- 05.1.Classes/tasks/list_twist/list_twist.py
+++ 05.1.Classes/tasks/list_twist/list_twist.py
@@ -3,14 +3,40 @@ import typing as tp
 
 
 class ListTwist(UserList[tp.Any]):
-    """
-    List-like class with additional attributes:
-        * reversed, R - return reversed list
-        * first, F - insert or retrieve first element;
-                     Undefined for empty list
-        * last, L -  insert or retrieve last element;
-                     Undefined for empty list
-        * size, S -  set or retrieve size of list;
-                     If size less than list length - truncate to size;
-                     If size greater than list length - pad with Nones
-    """
+    def __init__(self, data=None):
+        super().__init__(data if data is not None else [])
+
+    def __getattr__(self, name):
+        if name == 'reversed' or name == 'R':
+            return self.data[::-1]
+        elif name == 'first' or name == 'F':
+            if not self.data:
+                raise AttributeError("empty")
+            return self.data[0]
+        elif name == 'last' or name == 'L':
+            if not self.data:
+                raise AttributeError("empty")
+            return self.data[-1]
+        elif name == 'size' or name == 'S':
+            return len(self.data)
+        else:
+            raise AttributeError(f"no '{name}'")
+
+    def __setattr__(self, name, value):
+        if name == 'first' or name == 'F':
+            if not self.data:
+                raise AttributeError("empty")
+            self.data[0] = value
+        elif name == 'last' or name == 'L':
+            if not self.data:
+                raise AttributeError("empty")
+            self.data[-1] = value
+        elif name == 'size' or name == 'S':
+            if value < 0:
+                raise ValueError("size < 0")
+            if value < len(self.data):
+                self.data = self.data[:value]
+            elif value > len(self.data):
+                self.data.extend([None] * (value - len(self.data)))
+        else:
+                super().__setattr__(name, value)
