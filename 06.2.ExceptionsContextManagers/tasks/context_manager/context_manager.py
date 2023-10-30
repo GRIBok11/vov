0b82bd476a0acb3dee538b19b9408a2a77ea6798
duffily <rblashchuk@gmail.com>
2023-10-30 20:36:56 +0300

    context

--- 06.2.ExceptionsContextManagers/tasks/context_manager/context_manager.py
+++ 06.2.ExceptionsContextManagers/tasks/context_manager/context_manager.py
@@ -1,17 +1,35 @@
 from contextlib import contextmanager
-from typing import Iterator, TextIO, Type
+import sys
+import traceback
+import typing as tp
 
 
 @contextmanager
-def supresser(*types_: Type[BaseException]) -> Iterator[None]:
-    pass
+def supresser(*types_: tp.Type[BaseException]) -> tp.Iterator[None]:
+    try:
+        yield
+    except types_:
+        pass
 
 
 @contextmanager
-def retyper(type_from: Type[BaseException], type_to: Type[BaseException]) -> Iterator[None]:
-    pass
+def retyper(type_from: tp.Type[BaseException], type_to: tp.Type[BaseException]) -> tp.Iterator[None]:
+    try:
+        yield
+    except type_from as exception:
+        raise type_to(*exception.args)
 
 
 @contextmanager
-def dumper(stream: TextIO | None = None) -> Iterator[None]:
-    pass
+def dumper(stream: tp.TextIO | None = None) -> tp.Iterator[None]:
+    if stream is None:
+        stream = sys.stderr
+    try:
+        yield
+    except Exception as exception:
+        i = traceback.format_exception_only(type(exception), exception)
+        res = ""
+        for elem in i:
+            res += elem
+        stream.write(res)
+        raise
