e1381757be9f689385cec6ae4dab0b087862897b
duffily <rblashchuk@gmail.com>
2023-10-30 21:08:51 +0300

    vander

--- 03.2.NumpyAndPandas/tasks/vander/vander.py
+++ 03.2.NumpyAndPandas/tasks/vander/vander.py
@@ -8,3 +8,12 @@ def vander(array: npt.NDArray[np.float_ | np.int_]) -> npt.NDArray[np.float_]:
     :param array: input array,
     :return: vandermonde matrix
     """
+    array = np.asarray(array).ravel()
+    n = len(array)
+    result = np.zeros((n, n), dtype=float)
+    max_powers = n - 1 - np.arange(n)
+    result = np.power(array[:, np.newaxis], max_powers)
+    result = result[:, ::-1]
+
+    print("RESULT: ", result)
+    return result
