469e9318bb730c5f57e40f766b4e5022ae349493
duffily <rblashchuk@gmail.com>
2023-10-30 23:44:45 +0300

    time catcher

--- 06.2.ExceptionsContextManagers/tasks/time_catcher/time_catcher.py
+++ 06.2.ExceptionsContextManagers/tasks/time_catcher/time_catcher.py
@@ -1,2 +1,51 @@
 import time
 import contextlib
+
+import time
+
+
+class TimeoutException(BaseException):
+    pass
+
+
+class SoftTimeoutException(TimeoutException):
+    pass
+
+
+class HardTimeoutException(TimeoutException):
+    pass
+
+
+class TimeCatcher:
+    def __init__(self, soft_timeout=None, hard_timeout=None):
+
+        assert (soft_timeout is None or
+                hard_timeout is None or
+                hard_timeout >= soft_timeout)
+
+        if soft_timeout is not None:
+            assert soft_timeout > 0
+        if hard_timeout is not None:
+            assert hard_timeout > 0
+        self.tt = soft_timeout
+        self.td = hard_timeout
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __float__(self):
+        self.__exit__(None, None, None)
+        return self.rt
+
+    def __str__(self):
+        self.__exit__(None, None, None)
+        return f"Time consumed: {self.__float__():.4f}"
+
+    def __exit__(self, ar1, ar2, ar3):
+        self.rt = time.time() - self.start
+        if self.tt is not None and self.rt > self.tt:
+            raise SoftTimeoutException
+        if self.td is not None and self.rt > self.td:
+            raise HardTimeoutException
+        return
